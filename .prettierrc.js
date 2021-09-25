5611b802902eb67c8237d073de032042427eca07
Nag VBT <nagvbt@gmail.com>
2021-09-25 21:52:38 +0530

    Eslint and Prettier Setup

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+// .prettierrc.js
+
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  endOfLine: 'auto',
+  jsxSingleQuote: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+};
