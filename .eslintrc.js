9cc357a755f2a197f7913a370bf1dda3f6584ffe
Nag VBT <nagvbt@gmail.com>
2021-09-25 21:56:35 +0530

    Eslint Config

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    'react-native/react-native': true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'prettier',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:promise/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'promise',
+    'react',
+    'react-native',
+    'react-hooks',
+    'prettier',
+    'import',
+  ],
+  rules: {
+    'import/newline-after-import': ['error', { count: 2 }],
+    'prettier/prettier': 'error',
+    'react/jsx-no-duplicate-props': [1, { ignoreCase: false }],
+    // prevent eslint to complain about the "styles" variable being used before it was defined
+    'no-use-before-define': ['error', { variables: false }],
+    'react-hooks/rules-of-hooks': 'error',
+    // ignore errors for the react-navigation package
+    'react/prop-types': [
+      'error',
+      { ignore: ['navigation', 'navigation.navigate'] },
+    ],
+  },
+};
