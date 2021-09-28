9e5aa7002ae493531259baf36128a5f381300099
Nag VBT <nagvbt@gmail.com>
2021-09-28 18:42:04 +0530

    Babel Module Resolver for Long Paths

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,25 @@
-module.exports = function config(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        require.resolve('babel-plugin-module-resolver'),
+        {
+          root: ['./src/'],
+          extensions: ['.js', '.jsx', '.tsx', '.ios.js', '.android.js'],
+          alias: {
+            // define aliases to shorten the import paths
+            _assets: './assets',
+            _features: './src/features',
+            _components: './src/ui-lib',
+            _atoms: './src/ui-lib/components/atoms',
+            _molecules: './src/ui-lib/components/molecules',
+            _organisms: './src/ui-lib/components/organisms',
+            _templates: './src/ui-lib/components/templates',
+          },
+        },
+      ],
+    ],
   };
 };
